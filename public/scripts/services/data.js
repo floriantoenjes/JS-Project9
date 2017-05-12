9453c3e6caf79fe43e5a5c458db5cced912b0ccd
Florian Tönjes <skatepainter@yahoo.de>
2017-05-12 08:28:06 +0200

    Start implement service methods

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -2,5 +2,36 @@ angular.module("app")
 
 .service("dataService", function ($http) {
 
+    this.getRecipes = function () {
+        $http.get("localhost:5000/api/recipes/");
+    };
 
+    this.getCategories = function () {
+        $http.get("localhost:5000/api/categories");
+    };
+
+    this.getFoodItems = function () {
+        $http.get("localhost:5000/api/foodItems/");
+    };
+
+    this.getRecipesForCategory = function (category) {
+        $http.get(`localhost:5000/api/recipes?category=${category}`)
+    };
+
+    this.getRecipe = function (id) {
+        $http.get(`localhost:5000/api/recipes/${id}`);
+    };
+
+
+    this.updateRecipe = function (id) {
+        $http.put(`localhost:5000/api/recipes/${id}`);
+    };
+
+    this.addRecipe = function () {
+        $http.post("localhost:5000/api/recipes/");
+    };
+
+    this.deleteRecipe = function (id) {
+        $http.delete(`localhost:5000/api/recipes/${id}`);
+    };
 });
